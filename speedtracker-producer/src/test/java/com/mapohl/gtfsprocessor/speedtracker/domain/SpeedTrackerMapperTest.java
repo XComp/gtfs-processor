f9f31832e6d81db246cc82ea36cd436fce1b6c32
Matthias Pohl <XComp@gmx.de>
2020-04-22 22:54:10 +0200

    Added extra test for parsing the linkpoints.

--- speedtracker-producer/src/test/java/com/mapohl/gtfsprocessor/speedtracker/domain/SpeedTrackerMapperTest.java
+++ speedtracker-producer/src/test/java/com/mapohl/gtfsprocessor/speedtracker/domain/SpeedTrackerMapperTest.java
@@ -1,11 +1,18 @@
 package com.mapohl.gtfsprocessor.speedtracker.domain;
 
 import com.google.common.collect.Lists;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static com.mapohl.gtfsprocessor.speedtracker.domain.SpeedTracker.DATE_TIME_FORMATTER;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,6 +20,42 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class SpeedTrackerMapperTest {
 
     private SpeedTrackerMapper testInstance = new SpeedTrackerMapper();
+
+    @DisplayName("Parse Strings to create LinkPoint lists")
+    @ParameterizedTest(name = "Parse \"{0}\"")
+    @MethodSource("getLinkPointTestData")
+    void parseLinkPoints(String input, double... expectedLatLon) {
+        List<LinkPoint> expected = Lists.newArrayList();
+        for (int i = 0; i < expectedLatLon.length; i += 2) {
+            expected.add(new LinkPoint(expectedLatLon[i], expectedLatLon[i+1]));
+        }
+
+        List<LinkPoint> actual = SpeedTrackerMapper.parseLinkPoints(input);
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> getLinkPointTestData() {
+        return Stream.of(
+                Arguments.arguments("", new double[0]),
+                Arguments.arguments("asd", new double[0]),
+                Arguments.arguments("4", new double[0]),
+                Arguments.arguments("40.63511", new double[0]),
+                Arguments.arguments("40.63511,", new double[0]),
+                Arguments.arguments("40.63511,-", new double[0]),
+                Arguments.arguments("40.63511,-7", new double[] {40.63511, -7.0}),
+                Arguments.arguments("40.63511,-74.19581", new double[] {40.63511, -74.19581}),
+                Arguments.arguments("40.63511,-74.19581 ", new double[] {40.63511, -74.19581}),
+                Arguments.arguments("40.63511,-74.19581 asd", new double[] {40.63511, -74.19581}),
+                Arguments.arguments("40.63511,-74.19581 40", new double[] {40.63511, -74.19581}),
+                Arguments.arguments("40.63511,-74.19581 40.63381", new double[] {40.63511, -74.19581}),
+                Arguments.arguments("40.63511,-74.19581 40.63381,", new double[] {40.63511, -74.19581}),
+                Arguments.arguments("40.63511,-74.19581 40.63381,-", new double[] {40.63511, -74.19581}),
+                Arguments.arguments("40.63511,-74.19581 40.63381, asd-74.19336,40.63086", new double[] { 40.63511, -74.19581}),
+                Arguments.arguments("40.63511,-74.19581 40.63381,asd -74.19336,40.63086", new double[] { 40.63511, -74.19581}),
+                Arguments.arguments("40.63511,-74.19581 asd 40.63381,-74.19336", new double[] { 40.63511, -74.19581}),
+                Arguments.arguments("40.63511,-74.19581 40.63381,-74.19336 40.63086", new double[] { 40.63511, -74.19581, 40.63381, -74.19336})
+        );
+    }
 
     @Test
     void map() {
