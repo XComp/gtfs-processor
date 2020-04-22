35f3fc0cd10b8bb853fb292320b9725d43798814
Matthias Pohl <XComp@gmx.de>
2020-04-22 23:03:20 +0200

    Reorganized TaxiRide configuration.

--- taxiride-producer/src/main/java/com/mapohl/gtfsprocessor/taxiride/configuration/TaxiRideConfiguration.java
+++ taxiride-producer/src/main/java/com/mapohl/gtfsprocessor/taxiride/configuration/TaxiRideConfiguration.java
@@ -5,6 +5,7 @@ import com.mapohl.gtfsprocessor.genericproducer.domain.EntityMapper;
 import com.mapohl.gtfsprocessor.genericproducer.services.KafkaEmitService;
 import com.mapohl.gtfsprocessor.taxiride.domain.TaxiRide;
 import com.mapohl.gtfsprocessor.taxiride.domain.TaxiRideMapper;
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.LongSerializer;
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
@@ -19,15 +21,30 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 import java.util.Map;
 
 @Configuration
-public class KafkaProducerConfiguration {
+public class TaxiRideConfiguration {
 
     @Value(value = "${kafka.bootstrap-servers}")
-    private String kafkaBrokers;
+    private String bootstrapServersStr;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = Maps.newHashMap();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, this.bootstrapServersStr);
+
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic topic(@Value(value = "${kafka.topic}") String topicName,
+                          @Value(value = "${kafka.partition-count}") int partitionCount,
+                          @Value(value = "${kafka.replication-factor}") short replicationFactor) {
+        return new NewTopic(topicName, partitionCount, replicationFactor);
+    }
 
     @Bean
     public ProducerFactory<Long, TaxiRide> producerFactory() {
         Map<String, Object> configProps = Maps.newHashMap();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, this.kafkaBrokers);
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, this.bootstrapServersStr);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
