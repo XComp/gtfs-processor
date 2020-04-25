e25d3b913a50b9d17529758a75f9fe5dbdd90590
Matthias Pohl <XComp@gmx.de>
2020-04-25 10:23:15 +0200

    Updated data references.

--- tmux.sh
+++ tmux.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # initialize input data
-speedtrackers_filepath="$(readlink -f data/speedtrackers.sorted.csv.gz)"
-taxirides_filepath="$(readlink -f data/taxirides.sorted.csv.gz)"
+speedtrackers_filepath="$(readlink -f data/speedtrackers.2019.sorted.csv.gz)"
+taxirides_filepath="$(readlink -f data/taxirides.2019.sorted.csv.gz)"
 
 # initialize commands
 dummy_consumer_cmd="./gradlew dummy-consumer:bootRun -Pargs=\"--kafka.topic="
@@ -11,7 +11,7 @@ taxirides_consumer_cmd="${dummy_consumer_cmd}taxirides\""
 speedtrackers_consumer_cmd="${dummy_consumer_cmd}speedtrackers\""
 
 taxiride_producer_cmd="./gradlew taxiride-producer:bootRun --args=\"-c ${taxirides_filepath} -s 2019-01-01T00:00:00\""
-speedtracker_producer_cmd="./gradlew speedtracker-producer:bootRun --args=\"-c ${speedtrackers_filepath} -s 2017-05-05T09:44:00\""
+speedtracker_producer_cmd="./gradlew speedtracker-producer:bootRun --args=\"-c ${speedtrackers_filepath} -s 2019-01-01T00:00:00\""
 
 # run stop command killing all but the current pane
 if [[ "$1" == "stop" ]]; then
